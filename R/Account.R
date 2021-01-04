bc8ae51ad2fd38244615f0100dab000b388b03a8
Konrad Zdeb <konrad.zdeb@me.com>
2021-01-04 23:20:24 +0000

    Cleaning
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- R/Account.R
+++
@@ -1,7 +0,0 @@
-#' An S4 class to represent a bank account.
-#'
-#' @slot balance A length-one numeric vector
-#' @rdname Account-class
-Account <- setClass("Account",
-                    slots = list(balance = "numeric")
-)
